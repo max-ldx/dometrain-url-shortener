2b25f138631dc610828810117f8562d30b468837
max-ldx <242964282+max-ldx@users.noreply.github.com>
2026-08-14 15:03:11 +0200

    Add base62 encoding

---
+++ Api/tests/UrlShortener.Api.Core.Tests/Base62EncodingScenarios.cs
@@ -0,0 +1,18 @@
+﻿using UrlShortener.Core;
+
+namespace UrlShortener.Api.Core.Tests;
+
+public class Base62EncodingScenarios
+{
+    [Theory]
+    [InlineData(0, "0")]
+    [InlineData(1, "1")]
+    [InlineData(20, "K")]
+    [InlineData(1000, "G8")]
+    [InlineData(61, "z")]
+    [InlineData(987654321, "14q60P")]
+    public void ShouldEncodeNumberToBase62(int number, string expected)
+    {
+        number.EncodeToBase62().Should().Be(expected);
+    }
+}
