8397ecf9efc7e04b4cc1b56ffa014c5c7611c072
max-ldx <242964282+max-ldx@users.noreply.github.com>
2026-08-16 20:22:28 +0200

    Add token unicity

--- Api/src/UrlShortener.Core/TokenProvider.cs
+++ Api/src/UrlShortener.Core/TokenProvider.cs
@@ -2,14 +2,23 @@ namespace UrlShortener.Core;
 
 public class TokenProvider
 {
+    private readonly Lock _lock = new();
+    private long _token = 0;
     private TokenRange? _tokenRange;
 
-    public void AssignRange(int start, int end) => _tokenRange = new TokenRange(start, end);
+    public void AssignRange(int start, int end) => AssignRange(new TokenRange(start, end));
 
-    public void AssignRange(TokenRange tokenRange) => _tokenRange = tokenRange;
+    public void AssignRange(TokenRange tokenRange)
+    {
+        _tokenRange = tokenRange;
+        _token = tokenRange.Start;
+    }
 
     public long GetToken()
     {
-        return _tokenRange.Start;
+        lock (_lock)
+        {
+            return _token++;
+        }
     }
 }
