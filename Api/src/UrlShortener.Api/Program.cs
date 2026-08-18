59225da74a713cb6d1e303f597c5327a26a188e2
Maximilien Ledoux <242964282+max-ldx@users.noreply.github.com>
2026-08-18 10:05:29 +0200

    Add TokenRanges API worflow (#7)

--- Api/src/UrlShortener.Api/Program.cs
+++ Api/src/UrlShortener.Api/Program.cs
@@ -34,6 +34,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.MapGet("/", () => "API");
+
 app.MapPost("/api/urls",
     async (AddUrlHandler handler, AddUrlRequest request, CancellationToken cancellationToken) =>
     {
