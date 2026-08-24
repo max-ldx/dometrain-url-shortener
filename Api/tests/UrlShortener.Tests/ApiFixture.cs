917cba22f0aa296158a9f99dc148d964032259bb
max-ldx <242964282+max-ldx@users.noreply.github.com>
2026-08-24 19:15:25 +0200

    Add authentication

--- Api/tests/UrlShortener.Tests/ApiFixture.cs
+++ Api/tests/UrlShortener.Tests/ApiFixture.cs
@@ -1,7 +1,13 @@
+using System.Security.Claims;
+using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using UrlShortener.Api;
 using UrlShortener.Core.Urls.Add;
 using UrlShortener.Tests.Extensions;
@@ -14,14 +20,48 @@ public class ApiFixture : WebApplicationFactory<IApiAssemblyMarker>
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
         builder.ConfigureTestServices(services =>
-        {
-            services.Remove<IUrlDataStore>();
-            services.AddSingleton<IUrlDataStore>(new InMemoryUrlDataStore());
+            {
+                services.Remove<IUrlDataStore>();
+                services.AddSingleton<IUrlDataStore>(new InMemoryUrlDataStore());
 
-            services.Remove<ITokenRangeApiClient>();
-            services.AddSingleton<ITokenRangeApiClient, FakeTokenRangeApiClient>();
-        });
+                services.Remove<ITokenRangeApiClient>();
+                services.AddSingleton<ITokenRangeApiClient, FakeTokenRangeApiClient>();
+
+                services.AddAuthentication(defaultScheme: "TestScheme")
+                    .AddScheme<AuthenticationSchemeOptions, TestAuthHandler>("TestScheme", options => { });
+
+                services.AddAuthorizationBuilder()
+                    .SetDefaultPolicy(new AuthorizationPolicyBuilder()
+                        .RequireAuthenticatedUser()
+                        .Build())
+                    .SetFallbackPolicy(null);
+            }
+        );
 
         base.ConfigureWebHost(builder);
+    }
+}
+
+public class TestAuthHandler(
+    IOptionsMonitor<AuthenticationSchemeOptions> options,
+    ILoggerFactory logger,
+    UrlEncoder encoder)
+    : AuthenticationHandler<AuthenticationSchemeOptions>(options, logger, encoder)
+{
+    protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+    {
+        var claims = new[]
+        {
+            new Claim(ClaimTypes.Name, "Test user"),
+            new Claim("preferred_username", "max@test.com"),
+        };
+        var identity = new ClaimsIdentity(claims, "Test");
+        var principal = new ClaimsPrincipal(identity);
+        var ticket = new AuthenticationTicket(principal,
+            "TestScheme");
+
+        var result = AuthenticateResult.Success(ticket);
+
+        return Task.FromResult(result);
     }
 }
