83f0cc4298a912da9e62e6696e122408cb37b374
Maximilien Ledoux <242964282+max-ldx@users.noreply.github.com>
2026-09-06 11:02:50 +0200

    Add open telemetry

--- RedirectApi/src/UrlShortener.RedirectApi/Program.cs
+++ RedirectApi/src/UrlShortener.RedirectApi/Program.cs
@@ -1,8 +1,13 @@
 using Azure.Identity;
+using Azure.Monitor.OpenTelemetry.Exporter;
 using HealthChecks.CosmosDb;
 using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 using StackExchange.Redis;
 using UrlShortener.RedirectApi.Infrastructure;
 
@@ -26,6 +31,41 @@ builder.Services.AddHealthChecks()
 builder.Services.AddUrlReader(builder.Configuration["CosmosDb:ConnectionString"]!,
     builder.Configuration["DatabaseName"]!, builder.Configuration["ContainerName"]!,
     builder.Configuration["Redis:ConnectionString"]!);
+
+var applicationName = builder.Environment.ApplicationName ?? "RedirectApi";
+
+var telemetryConnectionString = builder.Configuration["APPLICATIONINSIGHTS_CONNECTION_STRING"];
+
+builder.Services.AddOpenTelemetry()
+    .ConfigureResource(resource => resource.AddService(serviceName: applicationName))
+    .WithTracing(tracing =>
+    {
+        tracing.AddSource("Azure.Cosmos.Operation");
+        tracing.AddHttpClientInstrumentation();
+        tracing.AddRedisInstrumentation();
+
+        tracing.AddConsoleExporter();
+        if (telemetryConnectionString is not null)
+        {
+            tracing.AddAzureMonitorTraceExporter(options => options.ConnectionString = telemetryConnectionString);
+        }
+    })
+    .WithMetrics(metrics =>
+    {
+        metrics.AddConsoleExporter();
+        if (telemetryConnectionString is not null)
+        {
+            metrics.AddAzureMonitorMetricExporter(options => options.ConnectionString = telemetryConnectionString);
+        }
+    })
+    .WithLogging(logging =>
+    {
+        logging.AddConsoleExporter();
+        if (telemetryConnectionString is not null)
+        {
+            logging.AddAzureMonitorLogExporter(options => options.ConnectionString = telemetryConnectionString);
+        }
+    });
 
 var app = builder.Build();
 
